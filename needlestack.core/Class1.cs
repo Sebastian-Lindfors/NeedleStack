4433e20ca322228a8f94dc65ae69b19bbbb1ac92
Sebastian Lindfors <sebastian.lindfors@academicworkacademy.se>
2025-02-04 11:51:56 +0100

    Solution for the needlestack problem

---
+++ needlestack.core/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace needlestack.core;
+
+public class Class1
+{
+
+    public static int NeedleStack(string haystack, string needle)
+    {
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (i + needle.Length > haystack.Length) return -1;
+
+            if (needle == haystack.Substring(i, needle.Length)) return i;
+        }
+
+        return -1;
+    }
+}
---
+++ needlestack.tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿namespace needlestack.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        string stack = "needlestack";
+        string needle = "needle";
+
+        int result = core.Class1.NeedleStack(stack, needle);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+            string stack = "stackneedle";
+        string needle = "needle";
+
+        int result = core.Class1.NeedleStack(stack, needle);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        string stack = "leetcode";
+        string needle = "leeto";
+
+        int result = core.Class1.NeedleStack(stack, needle);
+
+        Assert.Equal(-1, result);
+    }
+}
